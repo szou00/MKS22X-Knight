d9b03b70d6bda7d6582c125ea428145cc9a54c42
szou00 <szou00@stuy.edu>
2019-02-13 10:37:45 -0500

    added returns to the empty methods

--- KnightBoard.java
+++ KnightBoard.java
@@ -3,27 +3,37 @@ public class KnightBoard {
   /**
   *@throws IllegalArgumentException when either parameter is negative.
   */
-  public KnightBoard(int startingRows,int startingCols)
+  public KnightBoard(int startingRows,int startingCols) {
+
+  }
 
   //Initialize the board to the correct size and make them all 0's
 
 
-  public String toString()
+  public String toString() {
+    return "";
+  }
 
   /**
   *@throws IllegalStateException when the board contains non-zero values.
   *@throws IllegalArgumentException when either parameter is negative or out of bounds.
   */
-  public boolean solve(int startingRow, int startingCol)
+  public boolean solve(int startingRow, int startingCol) {
+    return false;
+  }
 
   /**
   *@throws IllegalStateException when the board contains non-zero values.
   *@throws IllegalArgumentException when either parameter is negative
    or out of bounds.
   */
-  public int countSolutions(int startingRow, int startingCol)
+  public int countSolutions(int startingRow, int startingCol) {
+    return 0;
+  }
 
-  private boolean solveH(int row ,int col, int level)
+  private boolean solveH(int row ,int col, int level) {
+    return false;
+  }
   // level is the # of the knight
 
 }
