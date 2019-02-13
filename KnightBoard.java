b9f69a047513f449dcceaf40716cb22dbe7e5c8e
Sharon Zou <szou00@stuy.edu>
2019-02-13 06:13:26 -0500

    added in empty methods

---
+++ KnightBoard.java
@@ -0,0 +1,29 @@
+public class KnightBoard {
+
+  /**
+  *@throws IllegalArgumentException when either parameter is negative.
+  */
+  public KnightBoard(int startingRows,int startingCols)
+
+  //Initialize the board to the correct size and make them all 0's
+
+
+  public String toString()
+
+  /**
+  *@throws IllegalStateException when the board contains non-zero values.
+  *@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  */
+  public boolean solve(int startingRow, int startingCol)
+
+  /**
+  *@throws IllegalStateException when the board contains non-zero values.
+  *@throws IllegalArgumentException when either parameter is negative
+   or out of bounds.
+  */
+  public int countSolutions(int startingRow, int startingCol)
+
+  private boolean solveH(int row ,int col, int level)
+  // level is the # of the knight
+
+}
